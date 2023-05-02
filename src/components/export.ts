04538100d7a26292abda7555ceeff3114b8b5cc5
SebasG819 <sebasgonzalez819@gmail.com>
2023-05-02 14:28:39 -0500

    3

---
+++ src/components/export.ts
@@ -0,0 +1,4 @@
+export { default as Btn_Log } from "./btn/buttons"
+export { default as Text_up } from "./CompText/index"
+export { default as Left } from "./Leftside/Leftside"
+export { default as Right } from "./Right/rightside"
