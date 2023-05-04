9ff8affa942fd2e87826da94f754e0569bda766d
SebasG819 <sebasgonzalez819@gmail.com>
2023-05-04 11:51:04 -0500

    7

---
+++ src/services/datares.ts
@@ -0,0 +1,21 @@
+export interface DataReg {
+
+    img : string
+    tittle: string
+
+}
+
+export const Regdata : DataReg [ ] = [
+
+    {img:"./images/User.png",
+    tittle:"Username"},
+    {img:"./images/Email.png",
+    tittle:"Email"},
+    {img:"./images/Lock.png",
+    tittle:"Password"},
+    {img:"./images/Lock.png",
+    tittle:"Confirm password"}
+    
+    
+
+]
